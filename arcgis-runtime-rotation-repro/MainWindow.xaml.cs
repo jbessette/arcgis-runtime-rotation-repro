65476f3b271bd8d09836057fed0e0221aeb9c31a
Joe Bessette <jbessette@bcs-gis.com>
2022-03-09 19:59:03 -0500

    Add project files.

---
+++ arcgis-runtime-rotation-repro/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.UI;
+using System;
+using System.Windows;
+
+namespace arcgis_runtime_rotation_repro
+{
+    public partial class MainWindow
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Initialize();
+
+            DataContext = this;
+
+            LocationDataSource = new RotationTestDataSource(33.826765, -116.539478);
+            LocationDataSource.HeadingChanged += async (s, e) =>
+            {
+                await MyMapView.SetViewpointRotationAsync(e);
+            };
+            LocationDataSource.StartAsync();
+        }
+
+        public RotationTestDataSource LocationDataSource { get; set; }
+
+        private void Initialize()
+        {
+            Esri.ArcGISRuntime.ArcGISRuntimeEnvironment.ApiKey = Properties.Settings.Default.ApiKey;
+            Esri.ArcGISRuntime.ArcGISRuntimeEnvironment.Initialize();
+
+            // Create new Map with basemap
+            Map myMap = new Map(BasemapStyle.ArcGISImagery);
+            myMap.LoadStatusChanged += (s, e) =>
+            {
+                if (e.Status == Esri.ArcGISRuntime.LoadStatus.Loaded)
+                {
+                    Dispatcher.Invoke(() => {
+                        MyMapView.LocationDisplay.DataSource = LocationDataSource;
+                        MyMapView.LocationDisplay.AutoPanMode = LocationDisplayAutoPanMode.Recenter;
+                        MyMapView.LocationDisplay.IsEnabled = true;
+
+                        LocationDataSource.Heading = 0;
+                    });
+                }
+            };
+
+            // Provide used Map to the MapView
+            MyMapView.Map = myMap;
+
+        }
+
+
+    }
+}
---
+++ arcgis-runtime-rotation-repro/RotationTestDataSource.cs
@@ -0,0 +1,54 @@
+﻿using Esri.ArcGISRuntime.Location;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arcgis_runtime_rotation_repro
+{
+    public class RotationTestDataSource : LocationDataSource, System.ComponentModel.INotifyPropertyChanged
+    {
+        public RotationTestDataSource(double latitude, double longitude)
+        {
+            MapPoint = new Esri.ArcGISRuntime.Geometry.MapPoint(longitude, latitude, Esri.ArcGISRuntime.Geometry.SpatialReferences.Wgs84);
+
+        }
+
+        private readonly Esri.ArcGISRuntime.Geometry.MapPoint MapPoint;
+        private double _heading;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+        public double Heading
+        {
+            get
+            {
+                return _heading;
+            }
+            set
+            {
+                _heading = value;
+                OnPropertyChanged();
+                this.UpdateHeading(_heading);
+                this.UpdateLocation(new Location(DateTimeOffset.Now, MapPoint, double.NaN, double.NaN, 10, _heading, false));
+
+            }
+        }
+
+        protected override Task OnStartAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        protected override Task OnStopAsync()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
